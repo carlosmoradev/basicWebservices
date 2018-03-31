ad166cfdd7207d8e9bdb752db5ce082fab302aca
Carlos Mario Mora Restrepo <github@carlosmora.biz>
2018-03-31 11:01:06 -0500

    agregando status.py

---
+++ status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import requests
+import sys
+host = 'localhost:8000'
+
+if len(sys.argv) > 1:
+    hito = sys.argv[1]
+else:
+    hito = None
+
+if hito == none:
+    r=requests.get('http://'+host+'/status')
+else:
+    r=requests.get('http://'+host+'/one'+hito)
+
+peint (r.status_code, "\n", r.text)
